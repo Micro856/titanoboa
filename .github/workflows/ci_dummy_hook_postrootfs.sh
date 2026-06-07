a28759510b64e9606dc9e619a3d1511d172c3127
Microwave <121560464+Micro856@users.noreply.github.com>
2026-06-07 17:22:06 +0200

    Update ci_dummy_hook_postrootfs.sh

--- .github/workflows/ci_dummy_hook_postrootfs.sh
+++ .github/workflows/ci_dummy_hook_postrootfs.sh
@@ -1,11 +1,64 @@
 #!/usr/bin/env bash
 
-# Variables
-imageref="$(jq -r '."image-ref"' </usr/share/ublue-os/image-info.json)"
-imageref="${imageref##*://}"
-imagetag="$(jq -r 'if ."image-branch" then ."image-branch" else ."image-tag" end' </usr/share/ublue-os/image-info.json)"
-sbkey='https://github.com/ublue-os/akmods/raw/main/certs/public_key.der'
+zypper addrepo --non-interactive -y "https://download.opensuse.org/repositories/home:/Microwave:/anaconda/openSUSE_Tumbleweed/home:Microwave:anaconda.repo"
+zypper install install -y anaconda-core anaconda-dracut anaconda-gui anaconda-live rsync
 
-# Secureboot Key Fetch
-mkdir -p /run/install/repo
-curl -Lo /run/install/repo/sb_pubkey.der "$sbkey"
+if [[ "${HIDE_SPOKE:-}" ]]; then
+    # Hide Root Spoke
+    cat <<EOF >>/etc/anaconda/conf.d/anaconda.conf
+[User Interface]
+hidden_spokes =
+    PasswordSpoke
+EOF
+fi
+
+tee /etc/anaconda/profile.d/opensuse-tumbleweed.conf <<'EOF'
+# Anaconda configuration file for openSUSE Tumbleweed
+
+[Profile]
+# Define the profile.
+profile_id = opensuse-tumbleweed
+
+[Profile Detection]
+# Match os-release values
+os_id = opensuse-tumbleweed
+
+[Network]
+default_on_boot = FIRST_WIRED_WITH_LINK
+
+[Bootloader]
+efi_dir = opensuse
+menu_auto_hide = False
+
+[Storage]
+file_system_type = ext4
+default_partitioning =
+    /     (min 5 GiB, max 70 GiB)
+    /var  (min 5 GiB)
+
+[User Interface]
+custom_stylesheet = /usr/share/anaconda/pixmaps/opensuse.css
+hidden_spokes =
+    NetworkSpoke
+    PasswordSpoke
+    UserSpoke
+	SubscriptionSpoke
+hidden_webui_pages =
+    anaconda-screen-accounts
+
+[Localization]
+use_geolocation = False
+EOF
+
+# Default Kickstart
+cat <<EOF >>/usr/share/anaconda/interactive-defaults.ks
+bootc --source-imgref=registry:ghcr.io/micro856/opensuse-bootc:latest
+%include /usr/share/anaconda/post-scripts/install-configure-upgrade.ks
+EOF
+
+# Signed Images
+cat <<EOF >>/usr/share/anaconda/post-scripts/install-configure-upgrade.ks
+%post --erroronfail
+bootc switch --mutate-in-place --enforce-container-sigpolicy --transport registry ghcr.io/micro856/opensuse-bootc:latest
+%end
+EOF
